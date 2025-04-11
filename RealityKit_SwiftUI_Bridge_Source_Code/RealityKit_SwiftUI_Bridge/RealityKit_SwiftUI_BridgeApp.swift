31d555f3e08e7bccdd0bbad2ef40fc4275946e08
Cong Le <cle155@csu.fullerton.edu>
2025-04-11 15:22:28 -0700

    1st try - working nicely

---
+++ RealityKit_SwiftUI_Bridge_Source_Code/RealityKit_SwiftUI_Bridge/RealityKit_SwiftUI_BridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealityKit_SwiftUI_BridgeApp.swift
+//  RealityKit_SwiftUI_Bridge
+//
+//  Created by Cong Le on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct RealityKit_SwiftUI_BridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
